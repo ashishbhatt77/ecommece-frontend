f40efb58168cdf7f3acc276255fa893bc4865d15
ashishbhatt77 <a7732808427@gmail.com>
2025-04-10 12:34:18 +0530

    first cosssmmit

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,9 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://ecommerce-eg35.onrender.com",
         changeOrigin: true,
-        secure: false
+        secure: true
       }
     }
   }
